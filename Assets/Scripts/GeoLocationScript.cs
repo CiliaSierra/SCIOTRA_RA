8e319f953a50ebf744ed149d41ee2e7b683994d0
CiliaSierra <cilia.ines.sierra@estudiant.upc.edu>
2020-01-15 19:25:05 +0100

    cilia

---
+++ Assets/Scripts/GeoLocationScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoLocationScript : MonoBehaviour {
+    public float latitude;
+    public float longitude;
+    
+ 
+}
